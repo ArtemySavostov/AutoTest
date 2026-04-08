e9b31e36d59a632b94a31e1b4e626697f3897294
ArtemySavostov <savostov322@yandex.ru>
2026-04-08 10:28:32 +0400

    First commit

---
+++ src/test/java/com/savostov/autotests/AutotestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savostov.autotests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/savostov/autotests/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.savostov.autotests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnOkStatus() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk());
+    }
+}
